ee7f9fb5239866fd7ea7c7179a4c278c07414905
mortonysus <255727@niuitmo.ru>
2023-01-17 05:34:20 +0300

    Err function implementation

--- solver.py
+++ solver.py
@@ -1,5 +1,10 @@
 import sympy as smp
 
+
+def Err(y_disk, y):
+    return 0.5 * sum([(point[1] - y.subs('t', point[0])) ** 2 for point in y_disk])
+
+
 if __name__ == '__main__':
     test_file_name = "simple_test.test"
     with open(test_file_name, 'r') as ist:
@@ -16,5 +21,8 @@ if __name__ == '__main__':
         ind_int = smp.integrate(sub_int_func, t)
         y = b * smp.exp(a * t) * (ind_int + c)
         c = smp.solve(y.subs('t', y_disc[1][0]) - y_disc[1][1], c)[0]
+        y = y.subs('c', c)
+
         print(y_disc[1][0], y_disc[1][1])
-        print(y_disc[1][0], y.subs({'t': y_disc[1][0], 'c': c}))
+        print(y_disc[1][0], y.subs({'t': y_disc[1][0]}))
+        print(Err(y_disc, y))
